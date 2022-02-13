cd063e23b5cb57ec7e78df4abd2cf48901775e9b
Jasper Lepardo <jsprlprd@gmail.com>
2022-02-13 22:34:23 +0800

    synv

---
+++ assets/js/functions.js
@@ -0,0 +1,13 @@
+
+  
+$(document).ready(function() {
+
+    var heading_height = $('header.fixed-top').innerHeight(); //get width automaticly
+
+    $('body').css('padding-top', heading_height);
+
+    // $('.js-table-scroller').css('max-height', 100vh - heading_height;
+
+});
+
+  
